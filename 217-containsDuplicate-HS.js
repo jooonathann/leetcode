8edb5aa0be19711f91652bceccead27e3e20b9d4
jooonathann <jooni.escobar.98@gmail.com>
2023-05-09 19:46:41 -0300

    217

---
+++ 217-containsDuplicate-HS.js
@@ -0,0 +1,15 @@
+// const containsDuplicate = function (nums) {
+//   const set1 = new Set(nums)
+//   return set1.size === nums.length ? false: true
+// };
+
+// /**
+
+const containsDuplicate = function (nums) {
+  const set1 = new Set();
+  for (let i = 0; i < nums.length; i += 1) {
+    if (set1.has(nums[i]) === true) return true;
+    set1.add(nums[i]);
+  }
+  return false;
+};
